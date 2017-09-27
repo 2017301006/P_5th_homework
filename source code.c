f3445348d51c0f887712691d11870cc391c9c5e2
2017301006 <31676266+2017301006@users.noreply.github.com>
2017-09-27 11:55:13 +0900

    5th Homework – Due on Midnight, Oct 8th
    Program the following:
    
    Suppose that we have the following matrices:
    A1 : 5 x 2
    A2 : 2 x 3
    A3 : 3 x 4
    A4 : 4 x 3
    A5 : 3 x 2
    A6 : 2 x 3
    Each matrix has all value of 1.
    Do the matrix multiplication A1 x A2 x A3 x A4 x A5 x A6.
    You have to show each matrix A1 to A6 first, then do the matrix multiplication, and finally show the result.

---
+++ source code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/** 5th Homework of Tejeong's Class **/
+
+/// Multiplication of matrices A1~A6 that have '1' elements all.
+
+int main() {
+
+	// Show each matrix A1~A6
+
+	printf("This program will multiply matrices A1~A6 that have '1' elements in themselves.");
+	printf("A1:\n\n1 1\n1 1\n1 1\n1 1\n1 1\n\n");	// Matrix A1 (5 by 2)
+	printf("A2:\n\n1 1 1\n1 1 1\n\n");	// Matrix A2 (2 by 3)
+	printf("A3:\n\n1 1 1 1\n1 1 1 1\n1 1 1 1\n\n");	// Matrix A3 (3 by 4)
+	printf("A4:\n\n1 1 1\n1 1 1\n1 1 1\n1 1 1\n\n");	// Matrix A4 (4 by 3)
+	printf("A5:\n\n1 1\n1 1\n1 1\n\n");	// Matrix A5 (3 by 2)
+	printf("A6:\n\n1 1 1\n1 1 1\n\n");	// Matrix A6 (2 by 3)
+
+	// Do the matrix multiplication A1xA2xA3xA4xA5xA6
+
+
+
+	// Show the result
+
+
+
+	getchar();
+	getchar();
+	return 0;
+}
